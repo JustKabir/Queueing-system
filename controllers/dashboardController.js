272e70a5855fc1769dc5a2a543a18bfe34fa4207
Abdul Kabir <akabir247@gmail.com>
2023-07-03 03:34:51 +0530

    adding nextToken API endpoint

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -17,8 +17,11 @@ exports.editDetails_patch = async(req, res)=>{
 // default page details
 exports.dashboard_get = async(req,res)=>{
     try {
+        let dashboardData = {};
+
         // fetching Org details
-        const org = await Models.organization.findone({where:{email:req.email}})
+        const org = await Models.organization.findone({where:{email:req.email}});
+
         //Queue length
         const queueLength = await Models.User.findOne({
             where:{
@@ -29,6 +32,9 @@ exports.dashboard_get = async(req,res)=>{
             },
             order:[['createdAt', 'DESC']]
         });
+
+        dashboardData['queueLength']= queueLength.tokenNo;
+
         // current no
        const currentNumber = await Models.User.findOne({
         where:{
@@ -38,18 +44,42 @@ exports.dashboard_get = async(req,res)=>{
             ]
         },
         order:[['createdAt', 'ASC']]
-       })
-       
+       });
+ 
+       dashboardData['currentNo'] = currentNumber;
+ 
+       return res.status(200).json({success:true, data:dashboardData})
+ 
     } catch (error) {
         console.error(error);
         return res.status(500).json({success:false,message:`Something went wrong, Please try again later`})
     }
 }
-// fetch count down
-// fetch help desk no
-
-
-// Enter Email
-
 
 // Next token button
+exports.nextToken_patch = async(req,res)=>{
+    try {
+        const org = await Models.organization.findOne({where:{email:req.email}});
+        
+        // check currentTokenNo is the last no
+        const lastTokenNo = await Models.User.findOne({
+            where:{
+                [Op.and]:[
+                {orgId:org.id},
+                    {attended:false}
+                ]
+            },
+            order:[['createdAt', 'DESC']]
+        });
+        console.log(lastTokenNo)
+        if(lastTokenNo.tokenNo<org.currentTokenNo+1) return res.status(404).json({success:false, message:"The queue is empty"});
+        
+        // Updating the currentTokenNo
+        await Models.organization.update({currentTokenNo:org.currentTokenNo+1}, {where:{id:org.id}});
+        return res.status(200).json({success:true,nextTokenNo:lastTokenNo.tokenNo, message:`next token no is: ${org.currentTokenNo +1}`})
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({success:false,message:`Something went wrong, Please try again later`})
+        
+    }
+}
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -10,7 +10,8 @@ const {organizationDetailsPatch} = require('../joiValidation/organization');
 
 
 // User Login
-router.patch('/edit',loginRequired,organizationDetailsPatch, dashboardController.editDetails_patch);
 router.get('/', loginRequired, dashboardController.dashboard_get);
+router.patch('/edit',loginRequired,organizationDetailsPatch, dashboardController.editDetails_patch);
+router.patch('/nexttoken',loginRequired, dashboardController.nextToken_patch);
 
 module.exports = router;
